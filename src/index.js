76b104973452771d8d201e304c9dd412b1a0a54e
Korben-Dallas-React <borozan.eugen@gmail.com>
2023-03-02 04:25:55 +0200

    remain 2 section

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
+import Swiper from "swiper";
+import "../node_modules/swiper/swiper.css";
+
 const burger = document.querySelector(".burger");
 const mobileNav = document.querySelector(".mobile-nav-wrapper");
 
 burger.addEventListener("click", (e) => {
   mobileNav.classList.toggle("mobile-nav--active");
 });
+
+// testimonial Swiper
+const swiper = new Swiper(".testimonial-slider", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 1,
+  loop: true,
+  spaceBetween: 30,
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 640px
+    800: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    }
+  },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+// testimonial Swiper
